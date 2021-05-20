3033d9a203d0cebe40727fda00ec327402c7da0b
Tuyen Le <tuyenle@Tuyens-Mac-mini.local>
2021-05-20 16:52:00 +1000

    Adding auth to our Serverless API

--- get.js
+++ get.js
@@ -1,19 +1,19 @@
-import handler from "./libs/handler-lib";
-import dynamoDb from "./libs/dynamodb-lib";
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
 
 export const main = handler(async (event, context) => {
-    const params = {
-        TableName: process.env.tableName,
-        Key: {
-            userId: "123",
-            noteId: event.pathParameters.id,
-        },
-    };
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id,
+    },
+  };
 
-    const result = await dynamoDb.get(params);
-    if(!result.Item) {
-        throw new Error("Item not found");
-    }
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error('Item not found');
+  }
 
-    return result.Item;
+  return result.Item;
 });
